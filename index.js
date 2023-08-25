9e5ab8d3cc6e9c9073d3fc267856757182282eb7
drjvtlkr <dheerajrvithalkar@gmail.com>
2023-08-25 18:18:18 +0530

    learn: variable declaration styles

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 // example of writing a external js doc
 
-let message;
-message = 'Hello';
-// or
+// let message;
+// message = 'Hello';
+// // or
 
-let mesage = 'hemlo';
+// let mesage = 'hemlo';
 
-alert(mesage);
-alert(message);
+// alert(mesage);
+// alert(message);
+// now adding more vriables
+
+// let user = 'john', age = 25, mesage = 'unga bunga' ;
+//  alert(user), alert (age), alert(mesage);
+// // end
+
+// new snippet
+
+// let message;
+
+// message = 'Hello!';
+
+// message = 'World!'; // value changed
+
+// alert(message);
+// end
+
+// new
+
+// let hello = 'Hello world!';
+
+// let message;
+
+// // copy 'Hello world' from hello into message
+// message = hello;
+
+// // now two variables hold the same data
+// alert(hello); // Hello world!
+// alert(message); // Hello world!
+
+// A variable should be declared only once. A repeated declaration of the same variable is an error:
+//  un comment the previous two snippets you will remember
